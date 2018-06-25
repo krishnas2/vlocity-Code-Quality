06e1e618bf8f8e5682b91df9f941438cab0a86b5
krishnas2 <krishnakanth1432@gmail.com>
2018-06-25 18:25:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -160,9 +160,10 @@ var RestCallMapper=(query,msg,opt,client)=>{
 	qryObj.end();
 };
 
-var typesubtypequery = name =>{
+function typesubtypequery  (name) {
 	var temp=name.lastIndexOf('_');
-	return "select+Id,Name+from+vlocity_cmt__OmniScript__c+where+vlocity_cmt__Type__c='"+name.substring(0,temp)"'+and+vlocity_cmt__SubType__c='"+name.substring(temp+1,)+"'";
+	console.log(name.substring(0,temp),name.substring(temp+1,));
+	return "select+Id,vlocity_cmt__PropertySet__c+from+vlocity_cmt__OmniScript__c+where+vlocity_cmt__Type__c='"+name.substring(0,temp)"'+and+vlocity_cmt__SubType__c='"+name.substring(temp+1,)+"'";
 }
 var OmniScriptperformop=(resp,client)=>{
 	var sample={};
